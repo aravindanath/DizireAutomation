0bfc4d2d8356d14dba97a1c90c19ae6e839eaa7b
Ganesh-Holla <ganeshhollaks31@gmail.com>
2020-03-14 13:22:11 +0530

    My First Push into Git

---
+++ src/test/java/fileHandling/ReadProperties_Ganesh.java
@@ -0,0 +1,26 @@
+package fileHandling;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadProperties_Ganesh {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		String rePath = System.getProperty("user.dir") + File.separator + "data" + File.separator + "config.properties";
+		System.out.println(rePath);
+
+		FileInputStream fis = new FileInputStream(rePath);
+		Properties pro = new Properties();
+		pro.load(fis);
+		
+		
+		System.out.println(pro.values());
+		
+	}
+
+}
