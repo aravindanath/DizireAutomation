9c55c7fc0e801199da0e8b1d8b1b8b1a834b98dd
Akshaykumar4196 <thalladaakshay696@gmail.com>
2020-03-14 11:42:36 +0530

    Akshayreadproperty file

---
+++ src/test/java/fileHandling/readproperties_Akshay.java
@@ -0,0 +1,25 @@
+package fileHandling;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class readproperties_Akshay {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String path = System.getProperty("user.dir")+File.separator+"data"+File.pathSeparator+"config.properties";
+		
+		FileInputStream fis = new FileInputStream(path);
+		Properties abc = new Properties();
+		
+		abc.load(fis);
+		String v = abc.getProperty("stagingurl");
+		
+		
+
+	}
+
+}
