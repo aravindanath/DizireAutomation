52999d8ac979cb8af6000e07ae8aaf9380dbae85
Arun-Raveendranathan <arun01791@gmail.com>
2020-03-15 11:56:50 +0530

    Test commit

--- src/test/java/fileHandling/ReadProperties_Arun.java
+++ src/test/java/fileHandling/ReadProperties_Arun.java
@@ -1,12 +1,17 @@
 package fileHandling;
 
 import java.io.*;
+import java.util.Properties;
 
 public class ReadProperties_Arun {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception{
 	
 	String path = System.getProperty("user.dir")+File.separator+"data"+File.separator+"config.properties";
 	System.out.println(path);
+	InputStream fi = new FileInputStream(path);
+	Properties p = new Properties();
+	p.load(fi);
+	System.out.print(p.getProperty("browser"));
 	
 	}
 	
