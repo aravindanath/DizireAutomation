d8c550a7ac191cbbf3109bb2b9c966b23bc38928
aravindanath <aravindanath86@gmail.com>
2020-04-05 12:04:41 +0530

    TestNG annotations and xml

---
+++ src/test/java/TestNG/Annotations.java
@@ -0,0 +1,24 @@
+package TestNG;
+
+
+import org.testng.annotations.Test;
+
+public class Annotations {
+
+
+    @Test
+    public void test1(){
+
+    System.out.println("test1 ");
+    }
+
+    @Test
+    public void test2(){
+
+        System.out.println("Test2");
+    }
+
+
+
+
+}
